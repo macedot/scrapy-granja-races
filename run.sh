236b13acc27b3bc40a891f2c517b5308a9f2dc06
Thiago Macedo <thpmacedo@gmail.com>
2017-03-10 17:49:57 -0300

    Add Log to file
    Add Log to file

--- run.sh
+++ run.sh
@@ -1,5 +1,56 @@
 #!/bin/bash
-BEGIN_RACE=$(cat ./lastRaceId)
-scrapy crawl granjaRaces -a begin=$BEGIN_RACE
-python ./granjaUpdateStatistics.py
+
+export PATH=".:$HOME:$PATH"
+
+declare baseName=$(basename $0)
+declare currentTime=$(date +%Y%m%d_%H%M%S)
+declare WORK_PATH="${HOME}/scrapyGranja"
+declare logFilePath="${WORK_PATH}/log/${baseName}-${currentTime}.log"
+
+function echoInfo {
+	local msg=$1
+	local DATETIME=$(date '+%Y-%m-%d %H:%M:%S')
+	local logMsg="${DATETIME} INFO [${baseName}] ${msg}"
+	echo $logMsg
+	[ -f ${logFilePath} ] && echo $logMsg >> ${logFilePath}
+	return 0
+}
+
+function echoError {
+	local msg=$1
+	DATETIME=$(date '+%Y-%m-%d %H:%M:%S')
+	logMsg="${DATETIME} ERROR [${baseName}] ${msg}"
+	echo $logMsg 1>&2
+	[ -f ${logFilePath} ] && echo $logMsg >> ${logFilePath}
+	return 0
+}
+
+# Get ready to work
+if [ ! -d "${WORK_PATH}" ]; then
+	echoError "Invalid WORK_PATH : ${WORK_PATH}" 1>&2
+	exit 1
+fi
+
+touch ${logFilePath}
+echoInfo "LOG: ${logFilePath}"
+
+cd $WORK_PATH
+BEGIN_RACE=$(cat ${WORK_PATH}/lastRaceId)
+PARAM=""
+if [ ! -z "${BEGIN_RACE}" ]; then
+	PARAM="-a begin=$BEGIN_RACE"
+fi
+echoInfo "================================================================================"
+echoInfo "scrapy crawl granjaRaces ${PARAM}"
+scrapy crawl granjaRaces ${PARAM} 2>&1 | tee -a ${logFilePath}
+
+echoInfo "================================================================================"
+echoInfo "python granjaUpdateStatistics.py"
+python $WORK_PATH/granjaUpdateStatistics.py 2>&1 | tee -a ${logFilePath}
+
+cd - &>/dev/null
+
+# Job is done!
+echoInfo "================================================================================"
+echoInfo "# Job is done!"
 exit 0
