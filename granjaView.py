866e5ca62a0be6bafd81ae45d94d8018f043db15
Thiago Macedo <thpmacedo@gmail.com>
2019-01-21 08:37:03 -0500

    - Fix for total number of laps - Missing tablesort files added

--- granjaView.py
+++ granjaView.py
@@ -99,6 +99,7 @@ class granjaView(object):
 ################################################################################
 if __name__ == '__main__':
 	cherrypy.config.update({'server.socket_host': '0.0.0.0'})
+	cherrypy.config.update({'server.socket_port': 80})
 	conf = {
 		'/': {
 			# # 'tools.sessions.on': True,
--- run.sh
+++ run.sh
@@ -4,8 +4,8 @@ export PATH=".:$HOME:$PATH"
 
 declare baseName=$(basename $0)
 declare currentTime=$(date +%Y%m%d_%H%M%S)
-#declare WORK_PATH="${HOME}/scrapyGranja"
-declare WORK_PATH="."
+declare WORK_PATH="${HOME}/scrapyGranja"
+#declare WORK_PATH="."
 declare logFilePath="${WORK_PATH}/log/${baseName}-${currentTime}.log"
 declare PYTHON="python3"
 
@@ -32,6 +32,8 @@ if [ ! -d "${WORK_PATH}" ]; then
 	echoError "Invalid WORK_PATH : ${WORK_PATH}" 1>&2
 	exit 1
 fi
+
+cd ${WORK_PATH}
 
 touch ${logFilePath}
 echoInfo "LOG: ${logFilePath}"
