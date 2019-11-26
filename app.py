b25e714fbe4cac620351a17c1cea66903afd60bc
Thiago Macedo <1750447+macedot@users.noreply.github.com>
2019-11-25 21:36:17 -0300

    gunicorn_logger

--- app.py
+++ app.py
@@ -77,4 +77,7 @@ def index():
 ################################################################################
 ################################################################################
 if __name__ == '__main__':
+	gunicorn_logger = logging.getLogger('gunicorn.error')
+	app.logger.handlers = gunicorn_logger.handlers
+	app.logger.setLevel(gunicorn_logger.level)
 	app.run()
